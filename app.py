3fe63c5e20be2b01ae79dfbfcbaea0cf3d705d04
whosekirito <whosekirito@gmail.com>
2025-08-11 23:42:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb'
-
+    return 'Hello from kirito'
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port
+    app.run(host="0.0.0.0", port=port)  # Bind to 0.0.0.0 so Render can detect it
